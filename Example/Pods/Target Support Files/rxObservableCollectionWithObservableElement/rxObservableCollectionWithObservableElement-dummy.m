9f07c4311168674608f8eabd5747ef9e9119c9a3
Alex <ismael.otero@allscripots.com>
2018-08-11 23:17:05 -0500

    firs commit

---
+++ Example/Pods/Target Support Files/rxObservableCollectionWithObservableElement/rxObservableCollectionWithObservableElement-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_rxObservableCollectionWithObservableElement : NSObject
+@end
+@implementation PodsDummy_rxObservableCollectionWithObservableElement
+@end
