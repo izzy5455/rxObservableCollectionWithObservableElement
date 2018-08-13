f26c67e7fef6bb93f6763c87ae340223c2f60250
Alex <ismael.otero@allscripots.com>
2018-08-13 14:18:28 -0500

    adding test

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,8 +1,26 @@
 import XCTest
+import RxSwift
 import rxObservableCollectionWithObservableElement
 
 class Tests: XCTestCase {
-    
+	
+	final class TestModel:NotifyChanged{
+		var elementChanged: PublishSubject<(keyPath: AnyKeyPath, old: Any, new: Any)>
+		
+		var string:String
+		var int:Int
+		var money: Double
+		init(string:String,int:Int, money:Double) {
+			self.string = string
+			self.int = int
+			
+			self.money = money
+			
+			
+			elementChanged = PublishSubject<(keyPath: AnyKeyPath, old: Any, new: Any)>()
+		}
+	}
+	
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,6 +35,27 @@ class Tests: XCTestCase {
         // This is an example of a functional test case.
         XCTAssert(true, "Pass")
     }
+	
+	
+	func testObservableInserts(){
+		let dp = DisposeBag()
+		
+		var obsCollection = ObservableCollection<TestModel>()
+		
+		var element = TestModel(string: "", int: 0, money: 0.0)
+		obsCollection.rx.subscribe(onNext: { (tuple) in
+			switch tuple.event{
+				case .insertedIndices(let indices):
+				assert(indices[0] == 0)
+				assert(tuple.element[0].money == element.money && tuple.element[0].string == element.string, "the elements are not the same" )
+				break
+				default:
+				assert(false, "this is not an inser event")
+			}
+		}).disposed(by: dp)
+		
+		obsCollection.append(element)
+	}
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
@@ -26,3 +65,6 @@ class Tests: XCTestCase {
     }
     
 }
+
+
+
